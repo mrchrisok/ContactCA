6809367bd85a0c8fb05ad1f0c6ddf8774c0b10fb
Osita Okonkwo <mrchrisok@hotmail.com>
2019-10-19 08:56:02 -0500

    Add project files.

---
+++ ContactCA.Api/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ContactCA.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ ContactCA.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ContactCA.Api.WebApiApplication" Language="C#" %>
